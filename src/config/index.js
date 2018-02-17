1a1bdd478df4ed58d99e53b2ee98e740020824fc
im100ruv <karnasaurabh123@gmail.com>
2018-02-17 12:17:28 +0530

    .

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 import mongodb from 'mongodb';
 
 export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": "mongodb://im100ruv:accessdenied@ds239368.mlab.com:39368/chattychatchat",
+  "port": 3005,
+  "mongoUrl": "mongodb://localhost:27017/chat-api",
+  // "port": process.env.PORT,
+  // "mongoUrl": "mongodb://im100ruv:accessdenied@ds239368.mlab.com:39368/chattychatchat",
   "bodyLimit": "100kb"
 }
